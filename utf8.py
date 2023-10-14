978e18ebaa1c00f3262a261bb0473658c07a5f78
schleising <stephen@schleising.net>
2023-10-14 12:29:58 +0100

    Tidy up

--- utf8.py
+++ utf8.py
@@ -1,26 +1,28 @@
 import json
-from pydantic import BaseModel, Field
+
+from pydantic import BaseModel
+from rich import print
 
 class IntegerField(BaseModel):
-    intIn: int
+    int_in: int
 
 class StringModel(BaseModel):
-    strIn: str
+    str_in: str
     integer: IntegerField
 
 # Create a string with a nbsp in it
-strIn = 'Non-breaking space: :-:🏈:'
+str_in = 'Non-breaking space: :-:🏈:'
 
-print(f'Unencoded                 : {strIn}')
-print(f'Encoded                   : {strIn.encode()}')
-print(f'Encoded Utf8              : {strIn.encode("utf-8")}')
-print(f'Encoded Unicode Escape    : {strIn.encode("unicode_escape")}')
-print(f'Encoded Raw Unicode Escape: {strIn.encode("raw_unicode_escape")}')
-print(f'Encoded Latin-1           : {strIn.encode("latin_1", errors="replace")}')
-print(f'Encoded Ascii             : {strIn.encode("ascii", errors="replace")}')
-print(f'Encoded Cp1252            : {strIn.encode("cp1252", errors="replace")}')
+print(f'Unencoded                 : {str_in}')
+print(f'Encoded                   : {str_in.encode()}')
+print(f'Encoded Utf8              : {str_in.encode("utf-8")}')
+print(f'Encoded Unicode Escape    : {str_in.encode("unicode_escape")}')
+print(f'Encoded Raw Unicode Escape: {str_in.encode("raw_unicode_escape")}')
+print(f'Encoded Latin-1           : {str_in.encode("latin_1", errors="replace")}')
+print(f'Encoded Ascii             : {str_in.encode("ascii", errors="replace")}')
+print(f'Encoded Cp1252            : {str_in.encode("cp1252", errors="replace")}')
 
-model = StringModel(strIn=strIn, integer=IntegerField(intIn=1))
+model = StringModel(str_in=str_in, integer=IntegerField(int_in=1))
 
 model_dump = model.model_dump()
 model_dump_json = model.model_dump_json()
@@ -34,12 +36,11 @@ print()
 
 # Create a dict similar to the pydantic model
 dictIn = {
-    'strIn': strIn,
+    'str_in': str_in,
     'integer': {
-        'intIn': 1
+        'int_in': 1
     }
 }
 
 print(dictIn)
 print(json.dumps(dictIn))
-print()
